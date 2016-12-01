d485d056fba24f5e95cfef0a65adbdebc5f1a99e
virusnik <seryowka@gmail.com>
2016-12-01 11:06:03 +0600

    add helpers app

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django_helpers',
+    version='0.0.1',
+    packages=['django_helpers', 'django_helpers.migrations'],
+    url='https://github.com/daniyarchambylov/django-helpers',
+    license='',
+    author='Daniyar Chambylov',
+    author_email='dan.chambylov@gmail.com',
+    description='Django helper modules'
+)
